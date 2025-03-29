df9b5f477fca24890141e83f019e2a8a583add3c
ayafounes <founesaya19@gmail.com>
2025-03-29 19:30:17 +0100

    Ajout de [description des modifications]

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import ClientDashboard from "./components/ClientDashboard";
 import ServiceRequestForm from "./pages/ClientRequestForm";
+import CardOuvrier from "./components/Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<ClientDashboard />} />
         <Route path="/ClientRequestForm" element={< ServiceRequestForm />} />
+        <Route path="/Card" element={< CardOuvrier />} />
       </Routes>
     </div>
   );
